6ffb6d5eb1120396f0a6f4c168927d5a8db25768
Pranav Sharma <psharma9@academic.rrc.ca>
2025-03-07 02:25:44 -0600

    Seeded database with sample customers

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+Customer.create!([
+  { full_name: "Kyle Geske", phone_number: "204-644-4444", email: "kgeske@rrc.ca", notes: "Loyal customer" },
+  { full_name: "Daisy Glutton", phone_number: "204-555-1234", email: "daisy.glutton@mailinator.com", notes: "Prefers email contact" },
+  { full_name: "Wally Glutton", phone_number: "204-555-9887", email: "wally.glutton@mailinator.com", notes: "Loves discounts" },
+  { full_name: "Missy Mismatch", phone_number: "204-555-2342", email: nil, notes: "Hasn't provided email" }
+])
