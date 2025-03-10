581cf1e70e906edb4cdf4c71434d6094819ce6bb
aryankecode <aryandesaiinfo@gmail.com>
2025-03-10 13:36:39 -0500

    Updated repository with new files

--- Gemfile
+++ Gemfile
@@ -34,13 +34,14 @@ gem "bootsnap", require: false
 gem 'sassc-rails'
 
 
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mswin mswin64 mingw x64_mingw ], require: "debug/prelude"
-
+  
   # Static analysis for security vulnerabilities [https://brakemanscanner.org/]
   gem "brakeman", require: false
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,10 @@
 #   end
 User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 Customer.create!([
-  { full_name: "Kyle Geske", phone_number: "204-644-4444", email: "kgeske@rrc.ca", notes: "Loyal customer" },
-  { full_name: "Daisy Glutton", phone_number: "204-555-1234", email: "daisy.glutton@mailinator.com", notes: "Prefers email contact" },
-  { full_name: "Wally Glutton", phone_number: "204-555-9887", email: "wally.glutton@mailinator.com", notes: "Loves discounts" },
-  { full_name: "Missy Mismatch", phone_number: "204-555-2342", email: nil, notes: "Hasn't provided email" }
+  { full_name: "Tony Stark", phone_number: "212-970-4133", email: "tony.stark@starkindustries.com", notes: "Brilliant inventor and industrialist" },
+  { full_name: "Steve Rogers", phone_number: "718-555-1941", email: "steve.rogers@avengers.com", notes: "Often misses old Brooklyn" },
+  { full_name: "Natasha Romanoff", phone_number: "917-555-7373", email: "n.romanoff@avengers.com", notes: "Highly skilled spy" },
+  { full_name: "Bruce Banner", phone_number: "415-555-1122", email: nil, notes: "Best not to make him angry" },
+  { full_name: "Thor Odinson", phone_number: "212-555-8769", email: "thor@asgard.com", notes: "Loves thunderstorms" }
 ])
+
